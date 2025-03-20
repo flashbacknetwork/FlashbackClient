0c83121d3da015b2717ec4d21bcb0cae32c7469a
mippl <mip.putalocura@gmail.com>
2025-03-20 21:20:39 +0100

    export httperror

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
-import { ApiClient } from './client';
+import { ApiClient, HttpError } from './client';
 import * as ApiTypes from './types/storage';
 import * as AuthTypes from './types/auth';
 import * as ApiInterfaces from './interfaces';
 
-export { ApiClient, ApiTypes, AuthTypes, ApiInterfaces };
+export { ApiClient, ApiTypes, AuthTypes, ApiInterfaces, HttpError };
