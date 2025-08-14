5c108cf2c50c4778af4ceffcb7ad601271619ffa
mippl <mip.putalocura@gmail.com>
2025-08-14 23:45:14 +0200

    remove v1 of the stellar client to avoid conflicts with v2

--- src/stellar/index.ts
+++
@@ -1,6 +0,0 @@
-import { StellarNetwork } from './transaction.js';
-import { FlashOnStellarClient, FlashOnStellarClientConfig } from './client.js';
-
-// Re-export types that might be needed by the consumer
-export type * from './models.js';
-export { StellarNetwork, FlashOnStellarClient, FlashOnStellarClientConfig };
