4cf3b5dd584b52539ae89574ef5cc4fb1f52a3ea
mippl <mip.putalocura@gmail.com>
2025-08-15 07:13:26 +0200

    fix withSignature decorator

--- src/stellarv2/utils/decorator.ts
+++ src/stellarv2/utils/decorator.ts
@@ -5,10 +5,21 @@ export function withSignature<T extends (...args: any[]) => Promise<any>>(
   method: T,
 ): T {
   return async function (
-    this: { getContext: () => ClientContext },
+    this: { getContext?: () => ClientContext; context?: ClientContext },
     ...args: Parameters<T>
   ): Promise<ReturnType<T>> {
-    const context = this.getContext();
+    // Try to get context from either getContext method or context property
+    let context: ClientContext;
+    if (this.getContext) {
+      context = this.getContext();
+    } else if (this.context) {
+      context = this.context;
+    } else {
+      throw new Error(
+        "FlashOnStellarClient: getContext method or context property is required for write operations",
+      );
+    }
+    
     if (!context.signTransaction) {
       throw new Error(
         "FlashOnStellarClient: signTransaction method is required for write operations",
@@ -26,9 +37,20 @@ export function withSignatureProperty<
   Object.defineProperty(target, propertyKey, {
     configurable: true,
     enumerable: true,
-    get(this: { getContext: () => ClientContext }) {
+    get(this: { getContext?: () => ClientContext; context?: ClientContext }) {
       return async (...args: Parameters<T>): Promise<ReturnType<T>> => {
-        const context = this.getContext();
+        // Try to get context from either getContext method or context property
+        let context: ClientContext;
+        if (this.getContext) {
+          context = this.getContext();
+        } else if (this.context) {
+          context = this.context;
+        } else {
+          throw new Error(
+            "FlashOnStellarClient: getContext method or context property is required for write operations",
+          );
+        }
+        
         if (!context.signTransaction) {
           throw new Error(
             "FlashOnStellarClient: signTransaction method is required for write operations",
